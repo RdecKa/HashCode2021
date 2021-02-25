0e5b28c11e965c2c5ce9272f871eb9c500485dac
Karin Frlic <kf5121@student.uni-lj.si>
2021-02-25 22:30:56 +0100

    Tailor to example e

--- qualification/Intersection.py
+++ qualification/Intersection.py
@@ -26,20 +26,43 @@ class Intersection:
 
     def scheduleWeighted(self, simulationLength):
         self.schedule = []
-        maxInterval = min(len(self.incoming) * 1, simulationLength)
-        totalWeight = sum(
-            [street.usedByCars for street in self.incoming])
-        for street in self.incoming:
-            if street.usedByCars == 0:
-                continue
-            weight = (street.usedByCars / totalWeight) * maxInterval
-            weight = min(weight, street.usedByCars)
-            # weight = 1
-            self.schedule.append((street, math.ceil(weight)))
 
-        # Sort
-        self.schedule = sorted(
-            self.schedule, key=lambda el: (el[0].numCarsStartingHere, el[0].length), reverse=True)
+        if (self.id != 499):
+            maxInterval = min(len(self.incoming) * 1, simulationLength)
+            totalWeight = sum(
+                [street.usedByCars for street in self.incoming])
+            for street in self.incoming:
+                if street.usedByCars == 0:
+                    continue
+                weight = (street.usedByCars / totalWeight) * maxInterval
+                weight = min(weight, street.usedByCars)
+                # weight = 1
+                self.schedule.append((street, math.ceil(weight)))
+
+            # Sort
+            self.schedule = sorted(
+                self.schedule, key=lambda el: (el[0].numCarsStartingHere, el[0].length), reverse=True)
+        else:  # 499
+            numStarting0 = []
+            numStarting1 = []
+            for street in self.incoming:
+                if street.numCarsStartingHere == 0:
+                    numStarting0.append(street)
+                else:
+                    numStarting1.append(street)
+
+            self.schedule = []
+            numStarting1 = sorted(
+                numStarting1, key=lambda el: el.numCarsStartingHere + el.length, reverse=True)
+            for street in numStarting1:
+                weight = max(street.numCarsStartingHere/2, 1)
+                self.schedule.append((street, math.ceil(weight)))
+
+            numStarting0 = sorted(
+                numStarting0, key=lambda el: el.length, reverse=False)
+            for street in numStarting0:
+                weight = 1
+                self.schedule.append((street, weight))
 
     def getScheduleString(self):
         if len(self.schedule) == 0:
